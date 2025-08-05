ad6a49d0d3b999b321921719fb4d3c9ffe5d3ef2
Purnendu-1425 <purnendueditograph@gmail.com>
2025-08-06 03:29:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('alertButton').addEventListener('click', function () {
+    alert('Welcome to interactive web development! 🚀');
+
+    this.style.transform = 'scale(0.95)';
+    setTimeout(() => {
+        this.style.transform = 'scale(1)';
+    }, 150);
+});
+
+
